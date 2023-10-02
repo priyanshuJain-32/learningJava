88301f3271f477d24265ba119e8bd7124c69875b
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-02 11:48:27 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,8 +1,25 @@
-public class Employee {
-    int eid;
-    String ename;
-    public void display(){
-        System.out.println("eid: "+eid);
-        System.out.println("ename: "+ename);
-    }
+import java.util.Scanner;
+
+class Employee{
+	    String ename;
+	    String eid;
+	    String edept;
+	    
+	    public Employee(){
+		        ename = "guest";
+	    }
+//Define the required methods
+        public Employee(String name, String id, String edept){
+            this.ename = name;
+            this.eid = eid;
+            this.edept = edept;
+        }
+        public void copyDept(Employee e){
+            this.edept = e.edept;
+        }
+        public void displayDetails(){
+            System.out.println("ename : " + this.ename);
+            System.out.println("eid : " + this.eid);
+            System.out.println("edept : " + this.edept);
+        }
 }
