256076ea1784fd0ad546858b392b8733f0112f88
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-02 11:47:35 +0530

    Create W2GA2.java

---
+++ W2GA2.java
@@ -0,0 +1,24 @@
+public class W2GA2 
+{
+	    public static void main(String args[]) 
+	    {
+		        Scanner s = new Scanner(System.in);
+		        Employee e1 = new Employee();
+		
+		        //Enter name of the employee
+		        String name = s.nextLine();
+		
+		        //Enter id of the employee
+		        String id = s.nextLine();
+		
+		        //Enter department of the employee
+		        String dept = s.nextLine();
+		
+		        Employee e2 = new Employee(name,id,dept);
+		
+		        e1.copyDept(e2); 
+		        //Copies the department name of e2 into e1's department name.
+		
+		        e1.displayDetails();
+	    }
+}
