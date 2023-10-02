999f5f0f8c3a61c68d841a0e3faea72b0328f64b
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-02 11:42:53 +0530

    Create FClass.java

---
+++ FClass.java
@@ -0,0 +1,12 @@
+public class FClass{
+	public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int w = Integer.parseInt(sc.nextLine());
+        int h = Integer.parseInt(sc.nextLine());
+        Rectangle r = new Rectangle();
+        r.setw(w);
+        r.seth(h);
+        int area = r.area();
+        System.out.print(area);
+    }
+}
