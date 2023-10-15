3879f0fe9d0acc4b2b2bcb7ddc631da52f1e24c8
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-15 19:11:07 +0530

    Experiment with Interfaces

---
+++ Interface1/B.java
@@ -0,0 +1,3 @@
+public interface B{
+         int b=20;
+   }
---
+++ Interface1/Example.java
@@ -0,0 +1,6 @@
+public class Example{
+      public static void main(String[] args) {
+            C oc=new C();
+            System.out.println(oc.b);
+      }
+}
