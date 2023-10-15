fc91cbaaa3d4fad0d6540edbedeb411bbffb6964
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-15 19:11:29 +0530

    Using Runnable Interface

---
+++ Runnable/A.java
@@ -0,0 +1,5 @@
+public class A implements Runnable{
+               public void run(){
+                         System.out.println("t2 thread");
+                   }
+    }
---
+++ Runnable/Example.java
@@ -0,0 +1,8 @@
+public class Example{
+             public static void main(String args[]){
+                     A obj=new A();
+                     Thread t2=new Thread();
+                     t2.start();
+                     System.out.println("Hello");
+                  }
+   }
