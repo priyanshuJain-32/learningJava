82106d961a22f67ffe7a3f43ca1d2dc83090c654
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-10-24 07:59:04 +0530

    Implemented Reflection with Fields

---
+++ W5 Generics, Erasure, Reflection/ReflectionCountFields/FClass.java
@@ -0,0 +1,50 @@
+import java.lang.reflect.*;
+class SampleClass{
+	private final int pr_data = 9;
+	private String pr_str;
+	public static int pu_data;
+	private SampleClass() {
+		//some code
+	}
+	public SampleClass(int pr_data_, String pr_str_) {
+	       pr_str = pr_str_;
+	}
+	public SampleClass(SampleClass tObj) {
+		this.pr_str = tObj.pr_str;
+	}
+	private boolean isValid() {
+		//some code
+		return true;
+	}
+	public int get_pr_data() {
+	       return pr_data;
+	}
+	public String get_pr_str() {
+               return pr_str;
+	}
+ }
+
+public class FClass{
+	public static void main(String[] args) {
+		SampleClass obj = new SampleClass(1,"ad");
+		Class<?> c = obj.getClass();
+		Field[] fields1 = c.getFields();
+		Field[] fields2 = c.getDeclaredFields();
+		for(Field f : fields1) {
+			System.out.print(f.getName() + " : ");
+			System.out.print(f.getType());
+			System.out.println();
+			System.out.println("Modifier: " +
+					Modifier.toString(f.getModifiers()));
+
+		}
+		for(Field f : fields2) {
+			System.out.print(f.getName() + " : ");
+			System.out.print(f.getType());
+			System.out.println();
+			System.out.println("Modifier: " +
+			Modifier.toString(f.getModifiers()));
+
+		}
+	}
+ }
