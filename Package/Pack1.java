f8d72230815226f87258fb619efc52792a099ef5
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-08 17:17:34 +0530

    Experiment with packages

---
+++ Package/Pack1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+package iitm;
+public class Pack1{
+    public void show(){
+        System.out.println("this is show");
+    }
+}
---
+++ Package/Pack4.java
@@ -0,0 +1,6 @@
+package util.iitm.program;
+public class Pack4{
+    public void show(){
+        System.out.println("This is show.");
+    }
+}
