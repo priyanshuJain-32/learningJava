56f0fe0bac4fa251f9470d40492011d650b2bfc7
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-13 16:53:12 +0530

    Add files via upload

---
+++ Week8/Var/FClass.java
@@ -0,0 +1,9 @@
+public class FClass{
+            public static void main(String[] args) {
+                   var a = 10;
+                   var b = "20";
+                   var c = a + b + 30;
+                   var d = a + 30 + b;
+                   System.out.println(c + ", " + d);
+            }
+      }
