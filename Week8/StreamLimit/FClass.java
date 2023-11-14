df819a282783aa21033807c60e3409f1041b0fd2
Priyanshu <117958306+priyanshuJain-32@users.noreply.github.com>
2023-11-14 10:48:17 +0530

    Add files via upload

---
+++ Week8/StreamLimit/FClass.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+import java.util.*;
+public class FClass{
+    public static void main(String[] args){
+        int m  = 15;
+        Stream.iterate(1, n->n+1)
+        .limit(m)
+        .filter(n -> m % n == 0)
+        .forEach(n -> System.out.print(n + " "));
+    }
+}
